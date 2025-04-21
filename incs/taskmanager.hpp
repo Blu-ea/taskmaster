d3afc1b380a9dcbb050c8944e0b3cbb0ca2fd7d3
Blue <82770859+Blu-ea@users.noreply.github.com>
2025-04-21 19:56:13 +0200

    Update: Start project and README.md

---
+++ incs/taskmanager.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Blu-ea on 21/04/25.
+//
+
+#ifndef TASKMANAGER_HPP
+#define TASKMANAGER_HPP
+
+#include <iostream>
+
+#endif //TASKMANAGER_HPP
---
+++ srcs/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Blu-ea on 21/04/25.
+//
+
+#include "../incs/taskmanager.hpp"
+#include "yaml-cpp/yaml.h"
+
+int main(){
+
+
+	std::cout << "Hello World TaskManager" << std::endl;
+
+}
